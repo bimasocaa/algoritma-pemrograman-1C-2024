605eabfad02723349a7e6fea0e2f2fd089a04f92
IM JOIS <71706340+o320@users.noreply.github.com>
2024-10-06 18:44:24 +0700

    240441100114-MUHAMMAD JOIS IAQUINTA-ISMA' YAFA NUR ZAMZAMI RAMADHANI-Modul 1 (#21)
    Co-authored-by: Unknown5456 <backup250506@gmail.com>

---
+++ modul 1/240441100114_MuhammadJoisIaquinta/soal_2.py
@@ -0,0 +1,25 @@
+s5 = 11
+s8_dan_s12 = 52
+# persamaan pertama dari s5
+# s5 = a + 4b = 11
+# persamaan kedua dari s8_dan_s12
+# s8_dan _s12 = 2a + 18b = 52
+
+# persamaan pertama
+# a = 11 - 4d
+# subtitusi ke persamaan kedua
+# 2(11 - 4d) + 18 = 52
+
+# menyederhanakan persamaan
+# 22 - 8b + 18b = 52
+# 10b = 30
+# b = 3
+
+d = 3
+a = 11 - 4 * d
+n = 8
+
+jumlah_8_suku_pertama = n / 2 * (2 * a + (n - 1) * d)
+#menampilkan hasil 
+
+print("jumlah 8 suku pertama:", jumlah_8_suku_pertama)
