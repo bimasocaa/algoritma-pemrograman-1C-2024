0edb1ba265970ef598cd5dbeb35613585247121e
Farhan Alamsyah <backup250506@gmail.com>
2024-10-06 18:43:42 +0700

    240441100150-Farhan Alamsyah-Isma' yafa Nur Zamzami Ramadhani-Modul 1 (#26)

---
+++ modul 1/240441100150-FarhanAlamsyah/Tugas praktikum soal 4.py
@@ -0,0 +1,9 @@
+
+# Masukkan library math
+import math
+
+#  Nyatakan variabelnya
+total = 7
+tim = 4
+kombinasi = math.comb(total, tim)
+print(f"Banyak cara untuk memilih 4 dari 7 orang: {kombinasi}")
