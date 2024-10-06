6bb0317e70cf6d81ead700022ba4096c6468b34f
ChelsySinaga <chelsysinaga44@gmail.com>
2024-10-06 18:41:45 +0700

    240441100146-CHELSY SINAGA-ISMA'YAFA NUR ZAMZAMI RAMADHANI-MODUL 1 (#31)

---
+++ Latihan/cobalagi.py
@@ -0,0 +1,28 @@
+print("berat badan saya adalah"+str(5))
+
+# untuk operator boolean
+a = True
+b = False
+
+hasil = a and b
+print(hasil)
+
+untuk operator logika
+x = True
+y = False
+
+hasil_and = x and y
+hasil_or = x or y
+hasil_not = x not y
+
+
+
+
+
+
+
+untuk union atau gabungan
+
+
+irisan 
+
---
+++ Latihan/tugas coding/DuaKondisi.py
@@ -0,0 +1,7 @@
+
+nilai = 90
+
+if nilai >= 90 :
+    print("anda mendapatkan nilai A")
+else :
+    print("anda mendapatkan nilai B")    
