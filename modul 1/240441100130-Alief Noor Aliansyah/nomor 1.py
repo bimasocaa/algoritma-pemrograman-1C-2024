a217e18faf43a37d69df4e324bbae4839a2e2f92
aliveexd <163909931+aliveexd@users.noreply.github.com>
2024-10-06 18:40:32 +0700

    240441100130_AliefNoorAliansyah_Modul1_AsprakKakIsma (#34)
    * coba git
    
    * 240441100130_AliefNoorAliansyah_Modul1_AsprakKakIsma
    
    * Delete modul 1/230441100130-Alief Noor Aliansyah directory
    
    * 240441100130_AliefNoorAliansyah_Modul1_AsprakKakIsma

---
+++ modul 1/240441100130-Alief Noor Aliansyah/nomor 1.py
@@ -0,0 +1,18 @@
+panjang = 20
+lebar = 13
+tinggi = 7 
+diameter = 14
+luas_selimut = 440
+jari_jari = diameter / 2
+pi = 22/7
+
+# rumus
+# luas selimut = 2*pi*r*t
+# 440 = 2*22/7*7*7
+# 440 = 44*7
+
+tinggi_tabung = 440 / 44 #hasilnya 10
+volume_balok = panjang * lebar * tinggi
+volume_tabung = pi * jari_jari**2 * tinggi_tabung
+print("Jadi hasil dari volume balok adalah", volume_balok)
+print("Jadi hasil dari volume tabung adalah", volume_tabung)
