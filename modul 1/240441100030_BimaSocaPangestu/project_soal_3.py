f7ac1aa3fa1b7660880c746489fea2cbeb86eb9c
bima <bimasocapanges7@gmail.com>
2024-10-15 23:45:40 +0700

    Delete modul 1 directory

--- modul 1/240441100030_BimaSocaPangestu/project_soal_3.py
+++
@@ -1,9 +0,0 @@
-# Diketahui
-uang_usd = 25
-perbandingan_usd_ke_idr = 15.102  # Contoh kurs, silakan sesuaikan dengan kurs sebenarnya
-
-# Menghitung nominal dalam IDR
-uang_idr = uang_usd * perbandingan_usd_ke_idr
-
-# Menampilkan hasil
-print(f"Nominal uang yang didapat Suraji: IDR {uang_idr}")
